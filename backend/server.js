98258659f1a49d0054c28428769638b465a0c42b
Medha Prasad <medha@teqfocus.com>
2024-03-19 11:34:55 +0530

    fixed cors error

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const snowflakeRoutes = require('./src/route/snowRoute.js');
+const cors = require('cors');
 const app = express();
 dotenv.config()
 
 const port = 8888;
 app.use(bodyParser.json());
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
+const snowflakeRoutes = require('./src/route/snowRoute.js');
 app.use('/api', snowflakeRoutes);
 
 app.listen(port, () => {
