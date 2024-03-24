b52e418b11cd64cc9e065af05db71eeaec56af12
RajnishPandeyTeqfocus <130439359+RajnishPandeyTeqfocus@users.noreply.github.com>
2024-03-24 21:28:22 +0530

    add patient details & patient by division table and visualization dashboard

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://public.tableau.com/javascripts/api/tableau-2.min.js"
+        ></script>
+        
+      </Head>
       <body>
         <Main />
         <NextScript />
